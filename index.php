f553f28dffd2a2d7f7e5e3435f4dfec29cb913db
Martin Dreher <luma.inc@gmail.com>
2017-10-17 19:05:18 +0200

    translated to english and added instructions

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Flugsuche</title>
-		<meta name="description" content="Beispiel Google QPX API">
+		<title>Flight Search</title>
+		<meta name="description" content="Example flight search using Google QPX API">
 		<meta name="keywords" content="HTML,CSS,XML,JavaScript">
 		<meta name="author" content="Laura Wölbeling">
 		<meta charset="UTF-8">
 	</head>
 	
 	<body>
-		<h1>Suche nach Flügen</h1>
+		<h1>Search for flights</h1>
 		<?php if (empty($_POST)) {?>
 			
 			<p> <form method="post">
-				Abflughafen:<br>
+				Origin airport:<br>
 				 <select name="origin">
 					<option value="BER">Berlin</option>
 					<option value="MUC">München</option>
@@ -22,51 +22,51 @@
 					<option value="CDG">Paris</option>
 					<option value="AMS">Amsterdam</option>
 				</select><br>
-				Zielflughafen:<br>
+				Destination airport:<br>
 				<select name="destination">
 					<option value="BER">Berlin</option>
 					<option value="MUC">München</option>
 					<option value="FRA">Frankfurt</option>
-					<option value="WDH" selected>Windhoek</option>
-					<option value="CDG">Paris</option>
+					<option value="WDH">Windhoek</option>
+					<option value="CDG" selected>Paris</option>
 					<option value="AMS">Amsterdam</option>
 				</select>
 				<br>
-				Datum Hinflug:<br>
-					<input type="date" name="hdate" value="2016-06-25"><br> <!-- funktioniert nur in Firefox -->
-				Datum Rückflug:<br>
-					<input type="date" name="rdate" value="2016-07-01"><br> <!-- funktioniert nur in Firefox -->
+				Depart:<br>
+					<input type="date" name="hdate" value="2017-10-25"><br>
+				Return:<br>
+					<input type="date" name="rdate" value="2017-11-01"><br>
 				<br><br>
-				<input type="submit" value="Suche">
+				<input type="submit" value="search">
 			</form></p>
 			
 		<?php } else {
-			echo "<p>Ihre Suche: ";
+			echo "<p>Your search: ";
 			//print_r($_POST);
-			echo "Flug (1 Erwachsener) von ".$_POST['origin'];
-			echo " nach ".$_POST['destination'];
-			echo " und zurück, ";
-			echo "Hinflug am ".date('d.m.Y', strtotime($_POST['hdate']));
-			echo ", Rückflug am ".date('d.m.Y', strtotime($_POST['rdate']));
+			echo "flight (1 adult) from ".$_POST['origin'];
+			echo " to ".$_POST['destination'];
+			echo " and back, ";
+			echo "departs ".date('d.m.Y', strtotime($_POST['hdate']));
+			echo ", returns ".date('d.m.Y', strtotime($_POST['rdate']));
 			echo "</p>";
 			?>
 			
-			<p><a href="index.php">Neue Suche</a></p>
-			<h2>Suchergebnisse</h2>
+			<p><a href="index.php">New search</a></p>
+			<h2>Results</h2>
 			<?php
 
-			// Eingaben an API senden und Ergebnis in Array speichern
+			// Send input to API and retrieve result in an array
 			$slices = array(array('origin' => $_POST['origin'], 'destination' => $_POST['destination'], 'date' => $_POST['hdate'])
 						  , array('origin' => $_POST['destination'], 'destination' => $_POST['origin'], 'date' => $_POST['rdate']));
 
 			$resultAsArray = getInformation($slices);
 
-			// Namen der Flughäfen den IATA-Codes zuordnen (alles in Ergebnis-Array enthalten)
+			// associate airport names with their IATA-Codes (data contained in the result array)
 			foreach ($resultAsArray['trips']['data']['airport'] as $city) {
 				$ap2[$city['code']] = $city['name'];
 			};
 
-			// nur Trip-Alternativen aus Ergebnis-Array filtern	
+			// filter trip-alternatives from the result	
 			$trips = array_filter($resultAsArray['trips']['tripOption'], function($kind) {
 				if (!isset($kind['kind'])) {
 					return false;
@@ -78,24 +78,21 @@
 			});
 
 
-				// Ausgabe der Suchergebnisse
+			// print results
 			foreach ($trips as $no => $trip) {
 				echo "<p><b>------- ALTERNATIVE " . ($no +1) . " ---------</b><br>\n";
-				echo "Preis: " . $trip['saleTotal'] . "<br>\n";
+				echo "Cost: " . $trip['saleTotal'] . "<br>\n";
 				foreach ($trip['slice'] as $index => $slice) {
-					if ($index == 0) echo "<b>Hinflug</b><br>\n";
-					else echo "<b>Rückflug</b><br>\n";
+					if ($index == 0) echo "<b>Depart</b><br>\n";
+					else echo "<b>Return</b><br>\n";
 					//print "Flug ". ($index + 1) .": " . $slices[$index]['origin'] . " nach " . $slices[$index]['destination'] . "\n<br>";
 					foreach ($slice['segment'] as $segment) {
-				
 						echo $segment['flight']['carrier'].$segment['flight']['number'].": ";
 						foreach ($segment['leg'] as $leg) {
-							
-						echo date('d.m.Y H:i', strtotime($leg['departureTime'])) . " <i>" . $ap2[$leg['origin']]. "</i> (". $leg['origin'] . ")";
+							echo date('d.m.Y H:i', strtotime($leg['departureTime'])) . " <i>" . $ap2[$leg['origin']]. "</i> (". $leg['origin'] . ")";
 							echo "    ---    ";
 							echo date('d.m.Y H:i', strtotime($leg['arrivalTime'])) . " <i>" . $ap2[$leg['destination']]. "</i> (". $leg['destination'] . ")";
 							echo "\n<br>";
-							
 						}
 					}
 				}
@@ -108,21 +105,24 @@
 
 <?php
 /**
-** Funktion zum Aufruf der QPX-API, gibt PHP-Array zurück
-**hier nach key= bitte euren Google Developer Key eintragen
-**/
-function getInformation($slices) {
-    $url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=";
+ * Calls the QPX-API using curl
+ **/
+function getInformation($slices) {	
+	include 'key.php';
+
+	if (!isset($key)) exit("Could not find an API key in key.php");
+	
+    $url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" . $key;
 
     $postData = '{
-                "request": {
-                    "passengers": {
-                        "adultCount": 1
-                        },
-                    "slice": ' . json_encode($slices) . ',
-					"solutions": 10
-                }
-            }';
+		"request": {
+			"passengers": {
+				"adultCount": 1
+				},
+			"slice": ' . json_encode($slices) . ',
+			"solutions": 10
+		}
+	}';
 
     $curlConnection = curl_init();
     curl_setopt($curlConnection, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
